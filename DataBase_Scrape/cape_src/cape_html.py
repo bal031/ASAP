bb9f9d04f6f7366de8a0c2b212c284f8a4b9e2d6
bal031 <59759642+bal031@users.noreply.github.com>
2020-05-22 04:55:41 -0700

    Added Database insertion, updated Departments
    Still need to check on server to make sure database interaction works

--- DataBase_Scrape/cape_src/cape_html.py
+++ DataBase_Scrape/cape_src/cape_html.py
@@ -1,30 +1,38 @@
 from bs4 import BeautifulSoup
 import requests, pickle
 import os
+import threading
+
+def main():
+    url = "http://cape.ucsd.edu/responses/Results.aspx?"
+    name = "Name="
+    crsNum = "CourseNumber="
+    path = "./Cape_HTML/"
+
+    headers = {"User-Agent":
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    }
+    #requests session that loads in cookies provided by a Selenium login, cookie only lasts an hour or so
+    s = requests.Session()
+    s.headers.update(headers)
+    with open('CapeCookies.txt', 'rb') as f:
+        s.cookies.update(pickle.load(f))
+    #print(s.cookies)
+    #Opens up Departments.txt and downloads the html pages for each departmentd
+    x = []
+    with open("Departments.txt") as f:
+        lines = f.readlines()
+        for department in lines:
+            x.append(threading.Thread(target=download_page, args=(path + department.rstrip() + '.html', url + name + "&" + crsNum + department, s)))
+            x[-1].start()
 
 
-url = "http://cape.ucsd.edu/responses/Results.aspx?"
-name = "Name="
-crsNum = "CourseNumber="
-path = "/Cape_HTML"
+def download_page(path, url, s):
+    page = s.get(url)
+    with open(path, 'wb') as f:
+        pickle.dump(page.text, f)
+    print(page.status_code)
 
-headers = {"User-Agent":
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-}
-#requests session that loads in cookies provided by a Selenium login, cookie only lasts an hour or so
-s = requests.Session()
-s.headers.update(headers)
-with open('CapeCookies.txt', 'rb') as f:
-    s.cookies.update(pickle.load(f))
-#print(s.cookies)
-#Opens up Departments.txt and downloads the html pages for each departmentd
-with open("Departments.txt") as f:
-    lines = f.readlines()
-    for department in lines:
-        page = s.get(url + name + "&" + crsNum + department)
-        with open(path + department.rstrip() + '.html', 'wb') as f:
-            pickle.dump(page.text, f)
-        print(page.status_code)
 
-#with open('Cape_Cse.txt', 'wb') as f:
-    #pickle.dump(page.text, f)
+if __name__ == "__main__":
+    main()
--- DataBase_Scrape/cape_src/cape_parser.py
+++ DataBase_Scrape/cape_src/cape_parser.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
 import os
 import re
+import Database
 
 #Class to hold courses and to organize the data so it can be sent to the database
 class CapeCourse:
-    def __init__(self, instructor, crs, term, enroll_num, rec_class, rec_instr, study_hrs, avg_grade_exp, avg_grade_rec):
+    def __init__(self, instructor, crs, term, response, enroll_num, rec_class, rec_instr, study_hrs, avg_grade_exp, avg_grade_rec):
         self.instructor = instructor
         self.crs = crs
         self.term = term
+        self.response = response
         self.enroll_num = enroll_num
         self.rec_class = rec_class
         self.rec_instr = rec_instr
@@ -32,6 +34,11 @@ def main():
         lines = f.readlines()
         for department in lines:
             parse_file(os.path.join(path,department.rstrip() + '.html'), results)
+    for result in results:
+        Database.insert_cape(result.crs.split()[0], results.crs.split()[1], result.instructor,
+            float(result.avg_grade_exp), float(result.avg_grade_rec), float(result.study_hrs),
+            float(result.rec_class.split()[0]), float(result.rec_instr.split()[0]), float(result.response), result.term,
+            Database.get_database())
 
 
 def parse_file(file, results):
@@ -44,22 +51,24 @@ def parse_file(file, results):
         #for each row parse the information and load it into the CapeCourse Class
         for course in courses:
             prof = course.find('td').contents[0]
-            crs = course.find('a', {
-                'id': re.compile(r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_hlViewReport')}).contents[0]
             term = course.find_all('td')[2].contents[0]
             enroll_num = course.find_all('td')[3].contents[0]
+            evals = course.find('span', {'id': re.compile(
+                r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblCAPEsSubmitted')}).contents[0]
+            crs = re.search('[A-Z]{2,4} [0-9]{1,3}', course.find('a', {'id': re.compile(
+                r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_hlViewReport')}).contents[0]).group(0)
             rec_class = course.find('span', {'id': re.compile(
                 r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblPercentRecommendCourse')}).contents[0]
             rec_instr = course.find('span', {'id': re.compile(
                 r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblPercentRecommendInstructor')}).contents[0]
-            study_hrs = course.find('span', {
-                'id': re.compile(r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblStudyHours')}).contents[0]
-            avg_grade_exp = course.find('span', {
-                'id': re.compile(r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblGradeExpected')}).contents[0]
-            avg_grade_rec = course.find('span', {
-                'id': re.compile(r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblGradeReceived')}).contents[0]
+            study_hrs = course.find('span', {'id': re.compile(
+                r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblStudyHours')}).contents[0]
+            avg_grade_exp = course.find('span', {'id': re.compile(
+                r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblGradeExpected')}).contents[0]
+            avg_grade_rec = course.find('span', {'id': re.compile(
+                r'ctl00_ContentPlaceHolder1_gvCAPEs_ctl[0-9]{1,10}_lblGradeReceived')}).contents[0]
             results.append(
-                CapeCourse(prof, crs, term, enroll_num, rec_class, rec_instr, study_hrs, avg_grade_exp, avg_grade_rec))
+                CapeCourse(prof, crs, term, float(evals, enroll_num), enroll_num, rec_class, rec_instr, study_hrs, avg_grade_exp, avg_grade_rec))
         print(file, ": ", len(results))
 
 
