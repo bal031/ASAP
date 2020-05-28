6c790ab5f6e51059e8446c0e7845297a9a979975
Nathan Krause <nkrause@ucsd.edu>
2020-05-28 23:55:49 +0000

    added user sessions

--- Backend/SocketServer.py
+++ Backend/SocketServer.py
@@ -1,6 +1,12 @@
 from aiohttp import web
 import socketio
 import sys
+sys.path.append('/home/nate/ASAP/Log')
+import LogASAP
+import json
+from time import sleep, time
+
+
 
 static_path = None
 # creates a new Async Socket IO Server
@@ -11,31 +17,82 @@ app = web.Application()
 # instance
 sio.attach(app)
 
+
+class User(): # implicit inheritance from object Python 3.x
+    """
+    defines a user object to store user information while connected to a socket. 
+    """
+    def __init__(self, sid, address, course_list=None, preferences=None, personal_events=None):
+        """
+        Parameters:
+            sid -- socket id
+            course_list -- dict of User course
+            preferences -- dict of User course preferences 
+        """
+        self.sid = sid # socket id 
+        self.address = address
+        self.course_list = course_list # User selected courses
+        self.preferences = preferences # User chosen preferences
+        self.personal_events = personal_events # User's personal events
+        self.schedule = None # set by generate schedule
+
 async def index(request):
     """
     handler for index GET requests
     """
-    print('Index requested')
+    print('Index requested by ', request.remote)
     with open(static_path+'index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
+@sio.on('connect')
+async def new_connection(sid, environ):
+    """
+    Sets up a User object for each session. A single computer can have multiple sessions and sockets open.
+    """
+    new_session = 'New session'
+    address = environ['aiohttp.request'].remote
+    await sio.save_session(sid, {'user': User(sid=sid, address=address), 'address' : address}) # save information about the user
+    
+    print(new_session + " for host: " + address)
+
+@sio.on('disconnect')
+async def remove_connection(sid):
+    session = await sio.get_session(sid=sid)
+    print('Connection ended: ' + session['address'] ) 
+
 @sio.on('message')
 async def print_message(sid, message):
+    """
+    Generic message handler mostly for testing
+    """
     # When we receive a new event of type
     # 'message' through a socket.io connection
     # we print the socket ID and the message
-    print("Socket ID: " , sid)
-    print(message)
+    print("Socket ID: " , sid, ' message: ', str(message))
 
-@sio.on('connect')
-async def new_connection(sid, message):
-    print("New connection from: " + sid)
 
 @sio.on('generate schedule')
-async def generate_schedule(sid, class_list):
-    sio.emit('processing')
-    # prcess the schedule
-    sio.emit('schedule done', 'replace this with a schedule')
+async def receive_schedule(sid, data):
+    """
+    event handler for when a User attempts to generate a schedule
+    """
+    session = await sio.get_session(sid)
+    print('Generate schedule reqeuest from: ', session['address'])
+    course_list = json.loads(data)
+    await generate_schedule(session['user'])
+    # sio.emit('processing')
+    # # prcess the schedule
+    # sio.emit('schedule done', 'replace this with a schedule')
+ 
+async def send_schedule(user: User):
+    await sio.emit('schedule ready', 'replace with schedule', room=user.sid)
+
+async def generate_schedule(user: User):
+    print("generating schedule for ", user.address)
+    sleep(5) # simulate processing 
+    await send_schedule(user)
+
+
 
 def main():
     """ 
@@ -43,15 +100,14 @@ def main():
     """
     # router
     global static_path
-    static_path = '/var/www/html/'
+    static_path = '/var/www/html/' # base bath to all of the static files. 
     app.router.add_get('/', index) # redirect to index on initial visit. 
     app.router.add_static('/', static_path) # automatically add routes for everything else
-   
     # app.add_routes([web.static('/', static_path)]) # this works 
     # We kick off our server
-    web.run_app(app, host='asap.ucsd.edu', port=80)
-    print('done')
-    # web.run_app(app) # localhost for debugging
+    web.run_app(app, host='asap.ucsd.edu', port=80) # this is blocking
+    # web.run_app(app) # localhost for debugging. Doesn't require root to run
 
 if __name__ == '__main__':
     main()
+
